43d23b56e7ab72613cb42b7340765fbc385c4db8
priyanksachdeva <sachdevapriyank1@gmail.com>
2025-05-30 22:04:44 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 10000,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
 // Middleware to decode JWT token and set user info for EJS templates
 app.use((req, res, next) => {
   const token = req.cookies.token;
